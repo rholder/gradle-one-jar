c786abcec3357ada46c63328ce41d7a19e08184b
Ray Holder <ray@blacklocus.com>
2013-01-13 02:02:49 -0600

    applying license

--- src/main/groovy/com/github/rholder/gradle/GradleOneJarPlugin.groovy
+++ src/main/groovy/com/github/rholder/gradle/GradleOneJarPlugin.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Ray Holder
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.rholder.gradle;
 
 import org.gradle.api.Plugin
@@ -46,7 +62,6 @@ import com.github.rholder.gradle.task.OneJar
  */
 class GradleOneJarPlugin implements Plugin<Project> {
 
-    // TODO comment all the things
     // TODO push to mavenCentral()
     // TODO test task lifecycle for sub-projects
     // TODO integrate external test project
